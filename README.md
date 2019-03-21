4ca8342768d250e89a53a39ef202d8bf5e921d4f
genzzzz <genesisf@gmail.com>
2019-03-21 19:21:58 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-qaycr6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-qaycr6)
---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { TodosComponent } from './component/todos/todos.component';
+import { TodoItemComponent } from './component/todo-item/todo-item.component';
+
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, TodosComponent, TodoItemComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
